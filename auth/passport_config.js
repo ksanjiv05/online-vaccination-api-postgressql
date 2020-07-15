4d57020650b887ace900d9daaf5e616402bd2a71
sanjiv kumar <ksanjiv0005@gmail.com>
2020-07-15 20:05:04 +0530

    initilal project after modify

--- auth/passport_config.js
+++
@@ -1,9 +0,0 @@
-const passport = require("passport");
-
-app.use(passport.initialize());
-app.use(passport.session());
-
-passport.use(UserDetails.createStrategy());
-
-passport.serializeUser(UserDetails.serializeUser());
-passport.deserializeUser(UserDetails.deserializeUser());
